9b5d424dab2464b3597f9a432fc9b28c030d5a5f
anthony <Anthony>
2026-02-02 16:56:26 +0000

    act

--- JavaPOO/ArrayListCarros/src/Carro.java
+++ JavaPOO/ArrayListCarros/src/Carro.java
@@ -20,6 +20,70 @@ public class Carro {
         this.velocidadeAtual = 0; // Inicia parado
     }
 
+    // Getters e Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(int velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public int getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+
+    public void setVelocidadeAtual(int velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    // Metodos de funcionamento
+    /**
+     * Acelera o carro aumentando a velocidade atual
+     * A velocidade atual não pode ultrapassar a velocidade máxima
+     * 
+     * @param incremento
+     */
+
+    public void acelerar(int incremento) {
+        // Verifica se o incremento é positivo
+        if (incremento <= 0) {
+            System.out.println("O incremento deve ser positivo");
+            return;
+        }
+
+        //Calcula a nova velocidade
+        int novaVelocidade = this.velocidadeAtual + incremento;
+
+        //Se ultrapassar a velocidade máxima, define a velocidade máxima
+        if (novaVelocidade > this.velocidadeMaxima) {
+            this.velocidadeAtual = this.velocidadeMaxima;
+            System.out.println("Atingiu a velocidade máxima de: " + this.velocidadeMaxima + " km/h");
+        } else {
+            this.velocidadeAtual = novaVelocidade;
+            System.out.println("Velocidade atual: " + this.velocidadeAtual + " km/h");
+        }
+
+    }
+
     
+
+
     
 }
